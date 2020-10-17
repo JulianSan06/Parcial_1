97dab648b7fbbaef7732baa3ca2c249e9a843172
Julián Sánchez <julian.sanchez6@udea.edu.co>
2020-10-17 16:26:33 -0500

    tercer commit implementacion del problema

--- Parcial_1/main.cpp
+++ Parcial_1/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+int tayectoria_X(int, int, int);
+int rayectoria_Y(int, int, int);
+
+
 int main()
 {
-     //COORDENADAS DE LOS CAÑONES
+     //COORDENADAS DE LOS CAÑONES----------------------------------------------------------
     int coordenadasO[2]={0,0}, coordenadasD[2];
+    int anguloO, anguloD;
 
     cout << "ingrese la altura del cañon ofensivo: " << endl;
     cin >> coordenadasO[1];
+    cout << "ingrese el angulo del cañon ofensivo: " << endl;
+    cin >> anguloO;
+    cout << "ingrese la velocidad inicial del cañon ofensivo: " << endl;
+
 
     for(int i = 0; i<2;i++){
         cout << "ingrese la coordenada " << i << " del cañon defensivo: " << endl;
@@ -16,7 +26,6 @@ int main()
     }
 
     int dis = coordenadasD[0];
-
     cout << endl;
     // CASOS------------------------------------------------------------------------------
 
@@ -27,12 +36,21 @@ int main()
         cout << "ingrese 2 para simular el caso 2" << endl;
         cout << "ingrese 3 para simular el caso 3" << endl;
 
-        cout << "ingrese 0 para salir" << '\n' << endl;
+        cout << "ingrese 0 para finalizar la simulacion" << '\n' << endl;
         cin >> op;
         switch (op) {
-        case 1:{}
-        case 2:{}
-        case 3:{}
+        case 1:{
+           //generar disparos (al menos tres) ofesnsivos que comprometan la integridad del cañon defensivo
+
+            for(int t = 1; t<=5; t++){
+                int x = trayectoria
+            }
+
+
 
         }
+        case 2:{}
+        case 3:{}
+        }
+    }
 }
